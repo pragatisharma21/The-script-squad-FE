63a443a3100c3069fd267a33d342c5330d5d685c
pragatisharma21 <pragatis212@gmail.com>
2025-02-06 22:24:40 +0530

    removed clerk

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { ThemeProvider } from "./context/ThemeProvider";
 import { AuthProvider } from "./context/AuthContext";
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
-}
 
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
 
@@ -22,9 +15,7 @@ createRoot(document.getElementById("root")).render(
       <AuthProvider>
         <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
           <BrowserRouter>
-            <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-              <App />
-            </ClerkProvider>
+            <App />
           </BrowserRouter>
         </GoogleOAuthProvider>
       </AuthProvider>
